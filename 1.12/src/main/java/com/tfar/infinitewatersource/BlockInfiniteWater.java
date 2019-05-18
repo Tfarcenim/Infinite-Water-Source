99250550fa850ac4284d045e56501fb30461c423
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-05-18 02:48:10 -0400

    cleanup code

--- 1.12/src/main/java/com/tfar/infinitewatersource/BlockInfiniteWater.java
+++ 1.12/src/main/java/com/tfar/infinitewatersource/BlockInfiniteWater.java
@@ -6,33 +6,21 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
-import net.minecraft.init.PotionTypes;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.potion.PotionUtils;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidActionResult;
-import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandler;
-import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.wrapper.InvWrapper;
 import net.minecraftforge.items.wrapper.PlayerInvWrapper;
 
 import javax.annotation.Nonnull;
@@ -40,8 +28,7 @@ import javax.annotation.Nullable;
 
 public class BlockInfiniteWater extends Block {
 
-  public static final String name = InfiniteWaterSource.MODID;
-  public static final ResourceLocation registryName = new ResourceLocation(InfiniteWaterSource.MODID, name);
+  public static final ResourceLocation registryName = new ResourceLocation(InfiniteWaterSource.MODID, InfiniteWaterSource.MODID);
 
   public BlockInfiniteWater() {
     super(Material.IRON);
@@ -50,17 +37,6 @@ public class BlockInfiniteWater extends Block {
     setHardness(5);
     setResistance(30);
   }
-
-/*  @Override
-  @SuppressWarnings("deprecation")
-  @Nonnull
-  public IBlockState getActualState(@Nonnull IBlockState state, IBlockAccess world, BlockPos pos) {
-    TileEntity tileEntity = world.getTileEntity(pos);
-    if (tileEntity instanceof TileInfiniteWater) {
-      return state;
-    }
-    return state;
-  }*/
 
   @Override
   public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
